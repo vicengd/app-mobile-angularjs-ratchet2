41ccdba5e40b902581f862eb63d4a38761d03e21
Vicente Garcia <vgarcia@brainstantsoup.com>
2014-03-11 13:33:08 +0100

    update item localstorage

--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,18 @@
 angular.module('app.controllers', [])
 .controller('TareasCtrl', function($scope, $http) {
-	if (localStorage.getItem('tareas')) {
-		$scope.tareas = JSON.parse(localStorage.getItem('tareas'));
+	if (localStorage.getItem('tareas-ratchet2')) {
+		$scope.tareas = JSON.parse(localStorage.getItem('tareas-ratchet2'));
 	}
 	else {
-	        $http.get('data/tareas.json').success(function(data) {
-	            localStorage.setItem('tareas', JSON.stringify(data));
+	        $http.get('data/tareas2.json').success(function(data) {
+	            localStorage.setItem('tareas-ratchet2', JSON.stringify(data));
 	            $scope.tareas = data;
 	        });
 	}
 })
 .controller('AddtareaCtrl', function($scope,$http) {
 	$scope.addtarea = function () {
-		var tareas = JSON.parse(localStorage.getItem('tareas'));
+		var tareas = JSON.parse(localStorage.getItem('tareas-ratchet2'));
 		var tarea = {
 			'id': tareas.length + 1,
 			'titulo': $scope.tarea.titulo,
@@ -20,13 +20,13 @@ angular.module('app.controllers', [])
 			'desclarga': $scope.tarea.desclarga
 		};
         tareas.push(tarea);
-        localStorage.setItem('tareas', JSON.stringify(tareas));
+        localStorage.setItem('tareas-ratchet2', JSON.stringify(tareas));
         location.href = '#/tareas';
 	};
 })
 .controller('TareaDetailCtrl', function($scope,$http,$stateParams) {
 	$scope.id = $stateParams.tareaId;
-	$scope.tareas = JSON.parse(localStorage.getItem('tareas'));
+	$scope.tareas = JSON.parse(localStorage.getItem('tareas-ratchet2'));
 	var index;
 	for (var i = 0; i < $scope.tareas.length; i++) {
 		if ($scope.tareas[i].id == $scope.id) {
@@ -39,7 +39,7 @@ angular.module('app.controllers', [])
 
    $scope.deletetarea = function() {
         $scope.tareas.splice(index,1);
-        localStorage.setItem('tareas', JSON.stringify($scope.tareas));
+        localStorage.setItem('tareas-ratchet2', JSON.stringify($scope.tareas));
         location.href = '#/tareas';
     };
 });
